fb738c4d8e1b3f4be49417a6d860ed5da941f1fc
StopReverseEngineeringMyScripts <78369650+StopReverseEngineeringMyScripts@users.noreply.github.com>
2021-02-09 16:31:34 +0000

    Update AreUSmart.lua

--- AreUSmart.lua
+++ AreUSmart.lua
@@ -1,7 +1,7 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/StopReverseEngineeringMyScripts/WhatAreYouDoingHere/main/YummySource",true))()
 
 local main = library:CreateWindow('Regular')
-local main2 = library:CreateWindow('Spawn & Despawn')
+local main2 = library:CreateWindow('Spawn')
 
 ------------------------------------------------------------------
 
