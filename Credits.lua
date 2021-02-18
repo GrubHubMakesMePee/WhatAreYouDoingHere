ccf90dba34f6b269c267930c428a86ae91ca55a2
StopReverseEngineeringMyScripts <78369650+StopReverseEngineeringMyScripts@users.noreply.github.com>
2021-02-18 18:46:10 +0000

    Create Credits.lua

---
+++ Credits.lua
@@ -0,0 +1,58 @@
+local Credits = s:Tab("Credits")
+
+Credits:Header('Credits:', function()
+end)
+
+Credits:Label('Boxking776#0001 - Head Developer - Support', function()
+end)
+
+Credits:Label('- Contribution to All GUIs & Discord Server -', function()
+end)
+
+Credits:Label(' ', function()
+end)
+
+Credits:Label('liloskiller#4269 - Developer - Support', function()
+end)
+
+Credits:Label('- Contribution to Prison Life & Small Scripts -', function()
+end)
+
+Credits:Label(' ', function()
+end)
+
+Credits:Label('JoshieGemFinder#6861 - Developer - Scripter', function()
+end)
+
+Credits:Label('- Big Contribution to Bee Swarm Simulator & Small Scripts -', function()
+end)
+
+Credits:Label(' ', function()
+end)
+
+Credits:Label('Vep#1003 - Contributor - Support', function()
+end)
+
+Credits:Label('- UI Library Help & Personal Support -', function()
+end)
+
+Credits:Label(' ', function()
+end)
+
+Credits:Label('dotexe#6677 - Head Administrator - Advertising', function()
+end)
+
+Credits:Label('- Server Contribution & Advertiser -', function()
+end)
+
+Credits:Label(' ', function()
+end)
+
+Credits:Label('Sakpot#5008 - Content Creator - Advertising', function()
+end)
+
+Credits:Label('- Youtuber & Showcaser -', function()
+end)
+
+Credits:Label(' ', function()
+end)
